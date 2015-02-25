ff76bbc29d82128bac7c947139bd2892cb6e2678
Omer Wazir <omer.wazir@gmail.com>
2015-02-25 15:59:41 -0700

    Index references compiled products

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function () {
+  var intro = document.getElementsByClassName("intro")[0];
+  intro.innerHTML = "Time is now " + Date();
+
+  //Figure out how to change the text to update every second.
+})();
