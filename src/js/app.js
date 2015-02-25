647a31762c934b1b925d9ab75d474cb418607e50
Omer Wazir <omer.wazir@gmail.com>
2015-02-25 15:59:26 -0700

    Put all source files in src

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+(function(){
+  let intro =  document.getElementsByClassName('intro')[0];
+  intro.innerHTML = `Time is now ${Date()}`;
+
+  //Figure out how to change the text to update every second.
+})()
